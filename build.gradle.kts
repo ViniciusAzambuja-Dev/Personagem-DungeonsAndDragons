43b29fedc6b3d917db6971d266c967eb34c34a45
Vinicius Azambuja <vinizambs@gmail.com>
2024-09-16 17:18:10 -0300

    Attempt 1: Fixed the build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    `maven-publish`
 }
 
 group = "org.example"
@@ -16,6 +17,24 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            groupId = "org.example" // Identificador do grupo
+            artifactId = "PersonagemDeD" // Nome do artefato
+            version = "1.0-SNAPSHOT" // Versão do artefato
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://jitpack.io") // URL do repositório onde o artefato será publicado
+        }
+    }
 }
